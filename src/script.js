151ee84142d190e5800b6b561afb217488b3912c
Castro <paula.castro@capgemini.com>
2024-06-27 12:13:53 -0300

    Break the app on purpose

--- src/script.js
+++ src/script.js
@@ -55,13 +55,7 @@ document.querySelector('button[type="submit"]')
     hideMessageAfterTimeout(successMessage)
   }, false)
 
-function showAndHideErrorMessage() {
-  const errorMessage = document.querySelector('.error')
-  errorMessage.style.display = 'block'
-  scroll(0,0)
-  hideMessageAfterTimeout(errorMessage)
-  return
-}
+
 
 function hideMessageAfterTimeout(element) {
   setTimeout(function() {
